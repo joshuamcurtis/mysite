ea47a5bf6fe889d050a7ba7bac8adb3499efcc5e
unknown <joshua.mi.curtis@gmail.com>
2023-03-28 10:06:57 -0400

    Created Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-published_date']
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
