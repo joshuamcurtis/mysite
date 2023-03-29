9bebba67b924b5406a7ad85d4160b5ef873ab74c
unknown <joshua.mi.curtis@gmail.com>
2023-03-28 20:24:08 -0400

    Added some html templates

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_date')
+    queryset = Post.objects.filter(status=1).order_by('-published_date')
     template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
