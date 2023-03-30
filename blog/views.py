25971f3d71cfb2990cb642d610338b6e79523220
unknown <joshua.mi.curtis@gmail.com>
2023-03-30 11:03:15 -0400

    Updated html and css

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-class PostList(generic.ListView):
+'''
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'author']
+    success_url = ''
+'''
+class PostListView(ListView):
     queryset = Post.objects.filter(status=1).order_by('-published_date')
     template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
+class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
